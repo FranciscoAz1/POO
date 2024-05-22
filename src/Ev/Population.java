4f81a64def8d067cf25fd693c8cd79a6e9ede0d6
FranciscoAz1 <azeredo2000@gmail.com>
2024-05-22 17:13:35 +0100

    changed Individuals to Individual. and added List of individuals to Population

--- src/Ev/Population.java
+++ src/Ev/Population.java
@@ -2,42 +2,42 @@ package ev;
 
 public class Population {
 
-	private int populationSize;
-	private int MaxPopulationSize;
+  private int populationSize;
+  private int MaxPopulationSize;
 
-	public void initializePopulation() {
-		// TODO - implement Population.initializePopulation
-		throw new UnsupportedOperationException();
-	}
+  public Population(int populationSize, int MaxPopulationSize) {
+    this.populationSize = populationSize;
+    this.MaxPopulationSize = MaxPopulationSize;
+  }
 
-	public void selectIndividuals() {
-		// TODO - implement Population.selectIndividuals
-		throw new UnsupportedOperationException();
-	}
+  public void selectIndividuals() {
+    // TODO - implement Population.selectIndividuals
+    throw new UnsupportedOperationException();
+  }
 
-	public void performCrossover() {
-		// TODO - implement Population.performCrossover
-		throw new UnsupportedOperationException();
-	}
+  public void performCrossover() {
+    // TODO - implement Population.performCrossover
+    throw new UnsupportedOperationException();
+  }
 
-	public void performMutation() {
-		// TODO - implement Population.performMutation
-		throw new UnsupportedOperationException();
-	}
+  public void performMutation() {
+    // TODO - implement Population.performMutation
+    throw new UnsupportedOperationException();
+  }
 
-	public void evaluateFitness() {
-		// TODO - implement Population.evaluateFitness
-		throw new UnsupportedOperationException();
-	}
+  public void evaluateFitness() {
+    // TODO - implement Population.evaluateFitness
+    throw new UnsupportedOperationException();
+  }
 
-	public void checkTerminationCondition() {
-		// TODO - implement Population.checkTerminationCondition
-		throw new UnsupportedOperationException();
-	}
+  public void checkTerminationCondition() {
+    // TODO - implement Population.checkTerminationCondition
+    throw new UnsupportedOperationException();
+  }
 
-	public void Epidemics() {
-		// TODO - implement Population.Epidemics
-		throw new UnsupportedOperationException();
-	}
+  public void Epidemics() {
+    // TODO - implement Population.Epidemics
+    throw new UnsupportedOperationException();
+  }
 
 }
