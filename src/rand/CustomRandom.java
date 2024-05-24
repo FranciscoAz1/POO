c6b9d8deb9082fecb344641ce08a23116d9d8064
FranciscoAz1 <azeredo2000@gmail.com>
2024-05-24 17:07:34 +0100

    asd

--- src/rand/CustomRandom.java
+++ src/rand/CustomRandom.java
@@ -3,16 +3,16 @@ package rand;
 import java.util.Random;
 
 public class CustomRandom {
-    private Random random;
+  private Random random;
 
-    public CustomRandom() {
-        random = new Random();
-    }
+  public CustomRandom() {
+    random = new Random();
+  }
 
-    // Método para gerar números exponenciais
-    public double nextExponential(double mean) {
-        return -mean * Math.log(random.nextDouble());
-    }
+  // Método para gerar números exponenciais
+  public double nextExponential(double mean) {
+    return -mean * Math.log(random.nextDouble());
+  }
 
-    // Outros métodos personalizados podem ser adicionados aqui
+  // Outros métodos personalizados podem ser adicionados aqui
 }
