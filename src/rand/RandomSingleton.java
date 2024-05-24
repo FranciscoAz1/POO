b92914fad089db1331b733ff20fc2096d5601c96
FranciscoAz1 <azeredo2000@gmail.com>
2024-05-24 10:51:19 +0100

    rand

--- src/rand/RandomSingleton.java
+++ src/rand/RandomSingleton.java
@@ -3,21 +3,21 @@ package rand;
 import java.util.Random;
 
 public class RandomSingleton {
-    private static RandomSingleton instance = null;
-    private Random random;
+  private static RandomSingleton instance = null;
+  private Random random;
 
-    private RandomSingleton() {
-        random = new Random();
-    }
+  private RandomSingleton() {
+    random = new Random();
+  }
 
-    public static RandomSingleton getInstance() {
-        if (instance == null) {
-            instance = new RandomSingleton();
-        }
-        return instance;
+  public static RandomSingleton getInstance() {
+    if (instance == null) {
+      instance = new RandomSingleton();
     }
+    return instance;
+  }
 
-    public Random getRandom() {
-        return random;
-    }
+  public Random getRandom() {
+    return random;
+  }
 }
