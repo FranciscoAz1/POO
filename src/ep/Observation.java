ee02deeec911904dae3e50c53b045537a2b3a210
RSN18 <ricardon64630@gmail.com>
2024-05-29 17:05:19 +0100

    Observações com o formato de saida correcto

--- src/ep/Observation.java
+++ src/ep/Observation.java
@@ -28,36 +28,31 @@ public class Observation extends AEvent implements IObservation {
   }
 
   @Override
-  public boolean HandleEvent() {
+public boolean HandleEvent() {
     StringBuilder sb = new StringBuilder();
-    sb.append("Observation " + this.counter + ":");
-    String topic1 = sb.toString();
 
-    sb = new StringBuilder();
-    sb.append(String.format("Present instant:\t\t%.2f\n", this.eventTime));
-    // sb.append(String.format("Number of realized events:\t%d\n",
-    // population.getRealizedEvents()));
-    sb.append(String.format("Population size:\t\t%d\n", population.getPopulation().size()));
-    sb.append(String.format("Number of epidemics:\t\t%d\n", population.getEpidemic().getCounter()));
-    sb.append(String.format("Best distribuition of the patrols:"));
-    sb.append(Best_Fitted_Individual.getBestIndividual().toString());
-    sb.append(
-        String.format("Empire policing time::\t\t%f\n", Best_Fitted_Individual.getBestIndividual().getPolicingTime()));
-    sb.append(String.format("Confort: \t\t%f\n", Best_Fitted_Individual.getBestIndividual().getConfort()));
-    sb.append(String.format("Other candidate distributions:"));
+    sb.append("Observation number: " + this.counter + "\n");
+    sb.append("Present instant:\t\t\t\t" + String.format("%.2f", this.eventTime) + "\n");
+    sb.append("Population size:\t\t\t\t" + population.getPopulation().size() + "\n");
+    sb.append("Number of epidemics:\t\t\t\t" + population.getEpidemic().getCounter() + "\n");
+    sb.append("Best distribution of the patrols:\t\t" + Best_Fitted_Individual.getBestIndividual().toString() + "\n");
+    sb.append("Empire policing time:\t\t\t\t" + String.format("%.2f", Best_Fitted_Individual.getBestIndividual().getPolicingTime()) + "\n");
+    sb.append("Comfort:\t\t\t\t\t" + String.format("%.2f", Best_Fitted_Individual.getBestIndividual().getConfort()) + "\n");
+    sb.append("Other candidate distributions:\n");
+
     List<Individual> otherCandidates = Best_Fitted_Individual.getBest5(population.getPopulation());
     for (int i = 0; i < otherCandidates.size(); i++) {
-      Individual candidate = otherCandidates.get(i);
-      sb.append(candidate.toString());
-      sb.append(String.format(": \t\t%f", candidate.getPolicingTime()));
-      sb.append(String.format(": \t\t%f", candidate.getConfort()));
+        Individual candidate = otherCandidates.get(i);
+        sb.append("\t\t\t\t\t\t" + candidate.toString() + " : " + String.format("%.2f", candidate.getPolicingTime()) + " : " + String.format("%.2f", candidate.getConfort()) + "\n");
     }
+
     this.counter++;
     // Program the event time for the next TimeEvent
     this.eventTime = (counter * finalInstance) / 20;
     System.out.println(sb.toString());
     return true;
-  }
+}
+
 
   public String indentString(String string, int spaces) {
     StringBuilder sb = new StringBuilder();
